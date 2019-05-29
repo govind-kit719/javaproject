a23a6835caff18484718f0c94104a3e27926208e
Govind <govindrao.penagati@kony.com>
2019-05-29 15:04:17 +0530

    even or odd logic implemented

--- GitTest/src/com/kony/test/TestGit.java
+++ GitTest/src/com/kony/test/TestGit.java
@@ -5,6 +5,12 @@ public class TestGit {
 	public static void main(String[] args) {
 		// print statement
 		System.out.println("Hello Kony");
+		int x =10;
+		if(x%2 == 0){
+			System.out.println("x is even");
+		}else{
+			System.out.println("x is odd");
+		}
 
 	}
 
